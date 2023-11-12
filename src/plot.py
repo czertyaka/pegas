730bc33102ab357ab98e972b8d9d35b9179ed425
czertyaka <pyhovoleg31@gmail.com>
2023-11-12 20:20:43 +0500

    print coordinates on click

--- src/plot.py
+++ src/plot.py
@@ -9,6 +9,10 @@ import contextily as cx
 import osmnx as ox
 
 
+def mouse_event(event):
+    print(f"{event.xdata};{event.ydata}")
+
+
 def create_plot(bounds):
     """Create figure to put axes on
 
@@ -22,6 +26,7 @@ def create_plot(bounds):
     x_margin = (bounds[2] - bounds[0]) / 20
     axes.set_ylim(bottom=bounds[1] - y_margin, top=bounds[3] + y_margin)
     axes.set_xlim(left=bounds[0] - x_margin, right=bounds[2] + x_margin)
+    cid = fig.canvas.mpl_connect('button_press_event', mouse_event)
     return (fig, axes)
 
 
